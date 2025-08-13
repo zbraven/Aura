a8ade16e59f354c3a4e13da688f9e5cd57a7b796
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2025-08-14 02:02:31 +0300

    Add weapon component to AuraCharacterBase
    Introduced a Weapon USkeletalMeshComponent to AAuraCharacterBase, attached to the WeaponHandSocket with collision disabled. Added new blueprint assets for Aura and Goblin Spear characters, updated character and enemy meshes, and modified the StartupMap to reflect these changes.

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -3,9 +3,14 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
